702f63d738f493e8bc71e3a011157e1f5afa4273
robgriffin247 <robgriffinwork@gmail.com>
2024-08-14 08:45:08 +0200

    draft of api_wan(), r2db() and return_table() functions (all saved in api_wan.R)

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# swapr
+# SWAPR
+
+SWAPR (*swapper*) is a project to demonstrate the use of R for data engineering. It will, among other things:
+
+1. Demonstrate how to extract data from the Star Wars API (**SWAPI**)
+1. Load SWAPI data into a DuckDB and/or Snowflake database
+1. Serve production ready datasets
+1. Utilise production ready datasets in a Shiny web app
+
+### Using R
+
+R is a statistical programming language, so it is designed around the use of data &mdash; data transformation, data visualisation and data analysis are all strengths of R. Data engineers will be most interested in the potential for transforming data, while the popularity of the language among data scientists could improve collaboration between these two user groups.
+
+R is an interpreted programming language, so you will need to install an interpreter. While you can download the interpreter and a basic command-line gui from the Comprehensive R Archive Netwrok (**CRAN**), most R developers are using a more feature-rich third-party development environment. Most popular in the R user community is [**RStudio**](https://posit.co/download/rstudio-desktop/) (currently rebranding to **Posit**, as it now does much more than just R).
+
+
+
--- api_wan.R
+++ api_wan.R
@@ -1,6 +1,12 @@
-library(httr2)
-library(data.table)
+setwd("~/git_personal/swapr/")
 
+library(data.table)
+library(duckdb)
+library(jsonlite)
+
+
+# api_wan(): makes a call to the swapi API to get all data for a given resource 
+#   (e.g. people, films, planets) and returns a data.table
 api_wan <- function(resource){
   
   url <- paste0("https://swapi.dev/api/", resource)
@@ -8,14 +14,43 @@ api_wan <- function(resource){
   
   while(!is.null(url)){
     message("Getting ", url)
-    resp <- req_perform(request(url))
-    resp_json <- resp_body_json(resp)
-    dt <- rbind(dt, rbindlist(lapply(lapply(resp_json$results, t), data.table)))
-    url <- resp_json$`next`
+    response <- fromJSON(url)
+    url <- response$`next`
+    dt <- rbind(dt, response$`results`)
   }
   
   dt
-    
 }
 
-api_wan("people")
+
+# r2db(): loads a data.table to a DuckDB database
+r2db <- function(dt, resource, schema, prefix){
+  # Load to duckdb
+  con <- dbConnect(duckdb(), dbdir="data/swapr.duckdb")
+  on.exit(dbDisconnect(con, shutdown = TRUE), add=TRUE)
+  dbExecute(con, paste0("CREATE SCHEMA IF NOT EXISTS ", toupper(schema)))
+  dbWriteTable(con, paste0(toupper(prefix), "_", toupper(resource)), dt, overwrite=TRUE)
+}
+
+
+# return_table(): returns a table from DuckDB (created as a function to prepackage
+#   the connection logic and on.exit() call; reducing input to a query)
+return_table <- function(query){
+  con <- dbConnect(duckdb(), dbdir="data/swapr.duckdb")
+  on.exit(dbDisconnect(con, shutdown = TRUE), add=TRUE)
+  data <- dbGetQuery(con, toupper(query))
+  data
+}
+
+
+# Demo:
+# - Download data from SWAPI
+starships <- api_wan("starships")
+
+# - Store data to DuckDB database
+r2db(starships, "starships", "staging", "stg")
+r2db(api_wan("films"), "films", "staging", "stg")
+
+# - Return table from DuckDB database
+return_table("select * from stg_films")
+return_table("show tables")
