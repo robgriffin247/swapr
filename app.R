0a2d2de5fea20a7f9f35db789328d9584dc227a9
robgriffin247 <robgriffinwork@gmail.com>
2024-08-14 10:34:18 +0200

    moved functions to tidy up

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 # Demo:
 setwd("~/git_personal/swapr/")
-source("api_wan.R")
+source("functions/api_wan.R")
+source("functions/r2db.R")
+source("functions/query_duckdb.R")
 
 # - Get data from SWAPI
 people <- api_wan("people")
@@ -9,11 +11,15 @@ starships <- api_wan("starships")
 # - Store data to DuckDB database
 r2db(starships, "starships", "staging", "stg")
 r2db(api_wan("films"), "films", "staging", "stg")
-sapply(c("films", #"people", "planets", 
-         "species", "starships", "vehicles"), function(r){
-           r2db(api_wan(r), r, "staging", "stg")
-         })
+#sapply(c("films", #"people", "planets", 
+#         "species", "starships", "vehicles"), function(r){
+#           r2db(api_wan(r), r, "staging", "stg")
+#         })
 
 # - Return tables from DuckDB database
 query_duckdb("show tables")
 query_duckdb("select * from stg_films")
+
+# - return films from episodes IV to VI
+films <- query_duckdb("select * from stg_films")
+films[episode_id %in% 4:6]
